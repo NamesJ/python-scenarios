f2400bab69f0329abc3a1fe4b43ae4550f835f56
J Sanders <sanders.j4@gmail.com>
2024-08-09 18:02:05 -0400

    Add scenarios for section 1

---
+++ 1. Basic Python Concepts/change_variable.py
@@ -0,0 +1,7 @@
+# Define a variable
+counter = 0
+
+# TODO: Change the value of 'counter' by adding 1 to it.
+# Example: counter = counter + 1
+
+# TODO: Print the new value of 'counter'.
---
+++ 1. Basic Python Concepts/use_variables.py
@@ -0,0 +1,11 @@
+# Define variables
+x = 10
+y = 5
+
+# TODO: Perform addition, subtraction, multiplication, and division
+# and store the results in new variables.
+
+# TODO: Create variable called `quite_a_few` that is equal to 100*x + 50*y/25
+# using the variables `x` and `y` on the right-hand-side to calculate the value.
+
+# TODO: Print the results
