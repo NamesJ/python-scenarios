c4f3bc993b2ebe405df40d711d56adab1d9c1711
J Sanders <sanders.j4@gmail.com>
2024-08-09 18:03:55 -0400

    add scenarios for modules and packages section

---
+++ 5. Modules and Packages/creating_packages/main.py
@@ -0,0 +1,11 @@
+# TODO: Import the 'concatenate' function from 'string_operations' module.
+
+# TODO: Call the 'concatenate' function and print the result.
+
+# TODO: Call the 'difference' function on some words and print the result
+
+strings = ['one', 'two', 'plenty', 'good', 'bad', 'wrong', 'evil', 'cool', 'flavor', 'cup', 'mug',
+           'keyboard', 'once upon a time', 'quick as a fox', 'not great, just okay']
+
+# TODO: Call the 'all_differences' function on the 'strings' list to get the differences for all
+# pairs of strings. Then print every pair of words and the difference between them using the result.
